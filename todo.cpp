7636c7e8b8026c1a50e6ade3f84fab43a9658b0a
Yathaartha <yuvicodes@gmail.com>
2024-06-17 22:37:47 +0530

    Added functions for Add, Deletion of task

--- todo.cpp
+++ todo.cpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -40,5 +41,55 @@ using namespace std;
 
 //Now a TO-DO list class
 class Todolist{
-    
+    private :
+    vector <Task> tasks;
+    public :
+
+    //displays menu for the options that the list does.
+
+    void displaymenu(){
+        cout<<"\n----------- TO DO LIST ----------\n"<<endl;
+        cout<<"1. Add task."<<endl;
+        cout<<"2. Remove task."<<endl;
+        cout<<"3. Display task."<<endl;
+        cout<<"4. Mark task as done."<<endl;
+        cout<<"5. Edit task details."<<endl;
+        cout<<"6. Exit TO-DO list."<<endl;
+    }
+
+    //Add a new task
+    void addtask(){
+        string name, description, due_date;
+        cout<< "Enter Task name : ";
+        cin.ignore();
+        getline(cin, name);         //inputs name of task
+        cout<<"Enter task description :"<<endl;
+        getline(cin, description);
+        cout << "Enter task due date (YYYY-MM-DD): ";
+        getline(cin, due_date);
+        tasks.emplace_back(name, description, due_date);
+        cout << "Task added successfully!" << endl;
+    }
+    //delete task
+    void deletetask(){
+        if (tasks.empty()) {
+            cout << "No tasks to delete!" << endl; //checks if task exist or not.
+            return;
+        }
+        cout << "Tasks:" << endl;
+        for (int i = 0; i < tasks.size(); ++i) {
+            cout << i + 1 << ". " << tasks[i].name<< endl;
+        }
+        cout << "Enter the task number to delete: ";
+        int taskNumber;
+        cin >> taskNumber;
+        if (taskNumber >= 1 && taskNumber <= tasks.size()) {
+            tasks.erase(tasks.begin() + taskNumber - 1);
+            cout << "Task deleted successfully!" << endl;
+        }
+        else {
+            cout << "Invalid task number!" << endl;
+        }
+    }
+
 };
