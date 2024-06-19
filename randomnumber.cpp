2e0e4f0ebdabdac415c6977a93932eb00e6af661
Yathaartha <yuvicodes@gmail.com>
2024-06-19 14:39:50 +0530

    Basic code structure written

--- randomnumber.cpp
+++ randomnumber.cpp
@@ -3,5 +3,41 @@
 //including the necessary files.
 #include<iostream>
 #include<cstdlib>
-#include<ctime>
+#include<time.h>
+
+using namespace std;
+
+class GuessNo {
+    public:
+    srand(time(NULL));
+    int number ;
+    number= rand()%1000+1;
+    int guess;
+    do {
+        cout<<"Enter your estimate : ";
+        cin<<guess;
+        int diff=abs(number-guess);
+        if (guess>number){
+            if(diff>50){
+                cout<<"Guess too high."<<endl;
+            }
+            else
+            cout<<"Guess is close but high."<<endl;
+        }    
+        else if(number>guess){
+            if(diff>50){
+                cout<<"Guess to low.";
+            }
+            else
+                cout<<"Guess close but low.";
+        }
+        else if (guess==number)
+        {
+            cout<<"YAY ! Correct number guessed";
+            return ;
+        }        
+    } while(guess!=number);    
+};
+
+
 
